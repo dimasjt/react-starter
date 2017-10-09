ce4d2c898fa01d257ae6e1e8c943cce448863973
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-10-09 16:30:07 +0700

    remove ConnectedRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Provider } from "react-redux"
-import { ConnectedRouter } from "react-router-redux"
 import PropTypes from "prop-types"
 
 // import { history } from "../store/createStore"
